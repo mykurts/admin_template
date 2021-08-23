636dc453591fbca0317da88456b9eac43d240952
Kurt Michael Auxtero <aux92.mykurts@gmail.com>
2021-08-23 15:17:31 +0800

    Adjustment.

--- template.rb
+++ template.rb
@@ -1,11 +1,7 @@
 require "fileutils"
 require "shellwords"
 
-# Copied from: https://github.com/mattbrictson/rails-template
-# Add this template directory to source_paths so that Thor actions like
-# copy_file and template resolve against our source files. If this file was
-# invoked remotely via HTTP, that means the files are not present locally.
-# In that case, use `git clone` to download them to a local temporary dir.
+
 def add_template_repository_to_source_path
 
   # Get source for template
